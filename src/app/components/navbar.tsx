89710c6ab99f5e4f8715d603578b4cfe1115dfdf
Yeiko <yeiko2003@gmail.com>
2023-06-13 22:30:29 -0600

    Arreglo

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,89 @@
+"use client"
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Navbar = () => {
+  const [showMaintenanceDropdown, setShowMaintenanceDropdown] = useState(false);
+  const [showAlmacenDropdown, setShowAlmacenDropdown] = useState(false);
+
+  const toggleMaintenanceDropdown = () => {
+    setShowMaintenanceDropdown(!showMaintenanceDropdown);
+  };
+
+  const toggleAlmacenDropdown = () => {
+    setShowAlmacenDropdown(!showAlmacenDropdown);
+  };
+
+  const routes = [
+    { href: '/maintenance', name: 'GPT', subroutes: ['Reactive', 'Preventive'] },
+    {href: '/calendario', name: 'Calendario'},
+    { href: '/almacen', name: 'Almacen', subroutes: ['Registro', 'Lista'] },
+    { href: '/registro', name: 'Registro' },
+    { href: '/contacto', name: 'Contacto' },
+  ];
+
+  return (
+    <nav className="flex items-center justify-between p-2" style={{ backgroundColor: 'rgb(53, 53, 53)' }}>
+      <div className="flex items-center">
+        <Image src="/bus1.jpg" alt="Logo de la empresa" width={50} height={50} className='rounded-full' />
+        <Link href="/">
+          <span className="ml-2 text-white text-lg font-bold cursor-pointer">Inicio</span>
+        </Link>
+      </div>
+      <div className="flex items-center">
+        {routes.map((route) => (
+          <div key={route.href} className="relative">
+            {route.name === 'Mantenimiento' ? (
+              <div className="relative">
+                <span
+                  className="text-white mr-4 cursor-pointer"
+                  onClick={toggleMaintenanceDropdown}
+                >
+                  {route.name}
+                </span>
+                {showMaintenanceDropdown && route.subroutes && (
+                  <div className="absolute z-10 bg-white rounded shadow-md py-2">
+                    {route.subroutes.map((subroute) => (
+                      <Link key={subroute} href={`/maintenance/${subroute.toLowerCase().replace(' ', '-')}`}>
+                        <span className="block text-gray-800 px-4 py-2 hover:bg-blue-100 cursor-pointer">
+                          {subroute}
+                        </span>
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ) : route.name === 'Almacen' ? (
+              <div className="relative">
+                <span
+                  className="text-white mr-4 cursor-pointer"
+                  onClick={toggleAlmacenDropdown}
+                >
+                  {route.name}
+                </span>
+                {showAlmacenDropdown && route.subroutes && (
+                  <div className="absolute z-10 bg-white rounded shadow-md py-2">
+                    {route.subroutes.map((subroute) => (
+                      <Link key={subroute} href={`/almacen/${subroute.toLowerCase().replace(' ', '-')}`}>
+                        <span className="block text-gray-800 px-4 py-2 hover:bg-blue-100 cursor-pointer">
+                          {subroute}
+                        </span>
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Link key={route.href} href={route.href}>
+                <span className="text-white mr-4 cursor-pointer">{route.name}</span>
+              </Link>
+            )}
+          </div>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
